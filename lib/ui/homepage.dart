e4e11760b4b911dbd91588fe20344b3f78aebae1
sam(TO) <sam0610@gmail.com>
2018-05-18 22:15:08 +0800

    tesr json

--- lib/ui/homepage.dart
+++ lib/ui/homepage.dart
@@ -5,6 +5,7 @@ import '../Helper/AnimatedPageRoute.dart';
 import '../Helper/firebase.dart';
 import '../Models/api.dart';
 import 'inspectionList.dart';
+import 'jsonTest.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
@@ -71,6 +72,15 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
     Navigator.of(context).push(
           new AnimatedRoute(
             builder: (_) => new InspectionRecord(),
+          ),
+        );
+  }
+
+  void _navigate2() {
+    Navigator.of(context).pop();
+    Navigator.of(context).push(
+          new AnimatedRoute(
+            builder: (_) => new JsonTest(),
           ),
         );
   }
@@ -160,6 +170,10 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
               new ListTile(
                 title: new Text('Go To Page 1'),
                 onTap: () => _navigate('/p1'),
+              ),
+              new ListTile(
+                title: new Text('Go To Page 2'),
+                onTap: () => _navigate2(),
               ),
               new Divider(),
               new ListTile(
